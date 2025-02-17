f0bacc7f7e5e3aad81cc2c2eb14dfacaf1814f54
Robertas Suminskas <robertas.suminskas@xprogram.co.uk>
2025-02-17 23:34:59 +0000

    Added Riboon and Controls

---
+++ ExcelAddIn/Presentation/Ribbon.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace ExcelAddIn
+{
+    partial class Ribbon : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Ribbon()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Ribbon));
+            this.tabAddins = this.Factory.CreateRibbonTab();
+            this.tabMyCoolStuff = this.Factory.CreateRibbonTab();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.button1 = this.Factory.CreateRibbonButton();
+            this.button2 = this.Factory.CreateRibbonButton();
+            this.grpDebug = this.Factory.CreateRibbonGroup();
+            this.grpDebugWindow = this.Factory.CreateRibbonButton();
+            this.grpWatchWindow = this.Factory.CreateRibbonButton();
+            this.rcbLogToDebugWindow = this.Factory.CreateRibbonCheckBox();
+            this.rcbEnableAppEvents = this.Factory.CreateRibbonCheckBox();
+            this.rcnDisplayEvents = this.Factory.CreateRibbonCheckBox();
+            this.rcbDisplayChattyEvents = this.Factory.CreateRibbonCheckBox();
+            this.grpHelp = this.Factory.CreateRibbonGroup();
+            this.grpDisplayAddinInfo = this.Factory.CreateRibbonButton();
+            this.btnToggleDeveloperMode = this.Factory.CreateRibbonButton();
+            this.tabAddins.SuspendLayout();
+            this.tabMyCoolStuff.SuspendLayout();
+            this.group1.SuspendLayout();
+            this.grpDebug.SuspendLayout();
+            this.grpHelp.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabAddins
+            // 
+            this.tabAddins.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tabAddins.Label = "TabAddins";
+            this.tabAddins.Name = "tabAddins";
+            // 
+            // tabMyCoolStuff
+            // 
+            this.tabMyCoolStuff.Groups.Add(this.group1);
+            this.tabMyCoolStuff.Groups.Add(this.grpDebug);
+            this.tabMyCoolStuff.Groups.Add(this.grpHelp);
+            this.tabMyCoolStuff.Label = "MyCoolStuff";
+            this.tabMyCoolStuff.Name = "tabMyCoolStuff";
+            // 
+            // group1
+            // 
+            this.group1.Items.Add(this.button1);
+            this.group1.Items.Add(this.button2);
+            this.group1.Label = "group1";
+            this.group1.Name = "group1";
+            // 
+            // button1
+            // 
+            this.button1.Label = "button1";
+            this.button1.Name = "button1";
+            // 
+            // button2
+            // 
+            this.button2.Label = "button2";
+            this.button2.Name = "button2";
+            // 
+            // grpDebug
+            // 
+            this.grpDebug.Items.Add(this.grpDebugWindow);
+            this.grpDebug.Items.Add(this.grpWatchWindow);
+            this.grpDebug.Items.Add(this.rcbLogToDebugWindow);
+            this.grpDebug.Items.Add(this.rcbEnableAppEvents);
+            this.grpDebug.Items.Add(this.rcnDisplayEvents);
+            this.grpDebug.Items.Add(this.rcbDisplayChattyEvents);
+            this.grpDebug.Label = "Debug";
+            this.grpDebug.Name = "grpDebug";
+            // 
+            // grpDebugWindow
+            // 
+            this.grpDebugWindow.Image = ((System.Drawing.Image)(resources.GetObject("grpDebugWindow.Image")));
+            this.grpDebugWindow.Label = "Debug Window";
+            this.grpDebugWindow.Name = "grpDebugWindow";
+            this.grpDebugWindow.ShowImage = true;
+            // 
+            // grpWatchWindow
+            // 
+            this.grpWatchWindow.Image = ((System.Drawing.Image)(resources.GetObject("grpWatchWindow.Image")));
+            this.grpWatchWindow.Label = "Watch Window";
+            this.grpWatchWindow.Name = "grpWatchWindow";
+            this.grpWatchWindow.ShowImage = true;
+            // 
+            // rcbLogToDebugWindow
+            // 
+            this.rcbLogToDebugWindow.Label = "Log to Debug Window";
+            this.rcbLogToDebugWindow.Name = "rcbLogToDebugWindow";
+            // 
+            // rcbEnableAppEvents
+            // 
+            this.rcbEnableAppEvents.Label = "Enable App Events";
+            this.rcbEnableAppEvents.Name = "rcbEnableAppEvents";
+            // 
+            // rcnDisplayEvents
+            // 
+            this.rcnDisplayEvents.Label = "Display Events";
+            this.rcnDisplayEvents.Name = "rcnDisplayEvents";
+            // 
+            // rcbDisplayChattyEvents
+            // 
+            this.rcbDisplayChattyEvents.Label = "Display Chatty Events";
+            this.rcbDisplayChattyEvents.Name = "rcbDisplayChattyEvents";
+            // 
+            // grpHelp
+            // 
+            this.grpHelp.Items.Add(this.grpDisplayAddinInfo);
+            this.grpHelp.Items.Add(this.btnToggleDeveloperMode);
+            this.grpHelp.Label = "Help";
+            this.grpHelp.Name = "grpHelp";
+            // 
+            // grpDisplayAddinInfo
+            // 
+            this.grpDisplayAddinInfo.Label = "Display AddinInfo";
+            this.grpDisplayAddinInfo.Name = "grpDisplayAddinInfo";
+            // 
+            // btnToggleDeveloperMode
+            // 
+            this.btnToggleDeveloperMode.Label = "Toggle Developer Mode";
+            this.btnToggleDeveloperMode.Name = "btnToggleDeveloperMode";
+            // 
+            // Ribbon
+            // 
+            this.Name = "Ribbon";
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Tabs.Add(this.tabAddins);
+            this.Tabs.Add(this.tabMyCoolStuff);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon_Load);
+            this.tabAddins.ResumeLayout(false);
+            this.tabAddins.PerformLayout();
+            this.tabMyCoolStuff.ResumeLayout(false);
+            this.tabMyCoolStuff.PerformLayout();
+            this.group1.ResumeLayout(false);
+            this.group1.PerformLayout();
+            this.grpDebug.ResumeLayout(false);
+            this.grpDebug.PerformLayout();
+            this.grpHelp.ResumeLayout(false);
+            this.grpHelp.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tabAddins;
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tabMyCoolStuff;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button2;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup grpDebug;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton grpDebugWindow;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton grpWatchWindow;
+        internal Microsoft.Office.Tools.Ribbon.RibbonCheckBox rcbLogToDebugWindow;
+        internal Microsoft.Office.Tools.Ribbon.RibbonCheckBox rcbEnableAppEvents;
+        internal Microsoft.Office.Tools.Ribbon.RibbonCheckBox rcnDisplayEvents;
+        internal Microsoft.Office.Tools.Ribbon.RibbonCheckBox rcbDisplayChattyEvents;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup grpHelp;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton grpDisplayAddinInfo;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton btnToggleDeveloperMode;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal Ribbon Ribbon
+        {
+            get { return this.GetRibbon<Ribbon>(); }
+        }
+    }
+}
---
+++ VisioAddIn/Presentation/Ribbon.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Office.Tools.Ribbon;
+
+namespace VisioAddIn
+{
+    public partial class Ribbon
+    {
+        private void Ribbon_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+            MessageBox.Show("Button1_Click");
+        }
+
+        private void button2_Click(object sender, RibbonControlEventArgs e)
+        {
+
+        }
+
+        private void grpDisplayAddinInfo_Click(object sender, RibbonControlEventArgs e)
+        {
+
+        }
+
+        private void btnToggleDeveloperMode_Click(object sender, RibbonControlEventArgs e)
+        {
+
+        }
+
+        private void grpDebugWindow_Click(object sender, RibbonControlEventArgs e)
+        {
+
+        }
+
+        private void grpWatchWindow_Click(object sender, RibbonControlEventArgs e)
+        {
+
+        }
+
+        private void rcbLogToDebugWindow_Click(object sender, RibbonControlEventArgs e)
+        {
+
+        }
+
+        private void rcbEnableAppEvents_Click(object sender, RibbonControlEventArgs e)
+        {
+
+        }
+
+        private void rcnDisplayEvents_Click(object sender, RibbonControlEventArgs e)
+        {
+
+        }
+
+        private void rcbDisplayChattyEvents_Click(object sender, RibbonControlEventArgs e)
+        {
+
+        }
+    }
+}
